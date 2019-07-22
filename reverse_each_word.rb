5d17dfa869aff5e8d0a618f48d8cad096207fbee
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-22 18:02:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 def reverse_each_word(string)
  
-string.split(" ").each 
-{|word| word.reverse!}.join(" ")
+string.split(" ").each {|word| word.reverse!}.join(" ")
 end
 
 def reverse_each_word(string)
-string.split(" ").collect 
-{|word| word.reverse!}.join(" ")
+string.split(" ").collect {|word| word.reverse!}.join(" ")
  end
