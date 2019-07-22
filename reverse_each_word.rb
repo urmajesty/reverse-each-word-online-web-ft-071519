6eebaf82322202d637d1e3f9f3c07ac3c1feebd0
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-22 17:30:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   
   new_array = string.split(" ")
- reversal = new_array.each {|a| a.reverse!}
+ reversal = new_array.each {|word| word.reverse!}
   return reversal.join(" ")
 end
