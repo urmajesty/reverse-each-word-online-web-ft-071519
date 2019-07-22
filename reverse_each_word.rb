f23ee33da017abbe25fc9e0ddd165ecad062c759
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-22 18:00:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,5 +4,5 @@ string.split(" ").each {|word| word.reverse!.join(" ")
 end
 
 def reverse_each_word(string)
- string.split(" ").collect {|x| x.reverse!}.join(" ")
+ string.split(" ").collect {|word| word.reverse!}.join(" ")
  end
