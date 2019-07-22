90deb723bb3c142970b2373a3e5260d0558162d8
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-22 16:47:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,3 @@
 def reverse_each_word(string)
   new_array = []
  split_it = string.split("")
-  string.size.times{new_array << split_it.pop}
-  new_array.join
-puts new_array
-end
-end
-
-end
-
-end
