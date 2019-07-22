d5bab49a7bcdab3f55d65d1f4a7f2d2444dc33b0
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-22 16:35:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
  split_it = string.split("")
   new_array = []
   string.size.times{new_array << split_it.pop}
-  new_array.join(5, 5, 6, 3, 3, 3, 4)
+  new_array.join
 
 
 
